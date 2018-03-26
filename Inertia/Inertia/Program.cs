148f1cc1d4de02490a5da25d5f2a5f3eb5e9942b
nigi4 <36262938+nigi4@users.noreply.github.com>
2018-03-26 10:24:23 +0300

    Add files via upload

--- Inertia/Inertia/Program.cs
+++ Inertia/Inertia/Program.cs
@@ -1,123 +1,154 @@
 ﻿using System;
 
-namespace Inertia
+namespace Test
 {
     internal class Program
     {
-        internal class Hero
+        class Matrix
         {
-            private int _x;
-            public int X
+            private static Element[,] _map;
+
+            public Matrix(int rows, int columns)
             {
-                set { _x = value; }
-                get { return _x; }
+                _map = new Element[rows, columns];
             }
             
-            private int _y;
-            public int Y
+            public Element this[int row, int column]
             {
-                set { _y = value; }
-                get { return _y; }
+                set { _map[row, column] = value; }
+                get { return _map[row, column]; }
+            }
+
+            public static Element GetElem(int x, int y)
+            {
+                return _map[y, x];
             }
             
-            private readonly string _shape;
-            public string Shape
+            public void Render()
             {
-                get { return _shape; }
+                Console.Clear();
+                for(int i = 0; i < _map.GetLength(0); i++)
+                {
+                    for (int j = 0; j < _map.GetLength(1); j++)
+                    {
+                        this[i, j] = i == 0 || i == _map.GetLength(0) - 1 ||
+                                    j == 0 || j == _map.GetLength(1) - 1 ?
+                                    (Element) new Wall(j, i) : new Ground(j, i);
+                        this[i, j].Display();
+                    }
+                }
             }
+        }
+        
+        abstract class Element
+        {
+            public string Shape { get; protected set; }
+            public int X { get; protected set; }
+            public int Y { get; protected set; }
+            public bool IsObstacle { get; protected set; }
 
-            public Hero(int x, int y, string shape)
+            public void Display()
             {
-                _x = x;
-                _y = y;
-                _shape = shape;
+                Console.SetCursorPosition(X, Y);
+                Console.Write(Shape);
             }
-
+        }
+        
+        class Hero : Element
+        {
+            public Hero(int x, int y)
+            {
+                X = x;
+                Y = y;
+                Shape = "ߐ";
+                IsObstacle = false;
+            }
+            
             public void Move(ConsoleKeyInfo press)
             {
                 switch (press.Key)
                 {
                     case ConsoleKey.UpArrow:
-                        Y--;
+                        while(!Matrix.GetElem(Y - 1, X).IsObstacle)
+                            Y--;
                         break;
                     case ConsoleKey.DownArrow:
-                        Y++;
+                        while(!Matrix.GetElem(Y + 1, X).IsObstacle)
+                            Y++;
                         break;
                     case ConsoleKey.LeftArrow:
-                        X--;
+                        while(!Matrix.GetElem(Y, X - 1).IsObstacle)
+                            X--;
                         break;
                     case ConsoleKey.RightArrow:
-                        X++;
+                        while(!Matrix.GetElem(Y, X + 1).IsObstacle)
+                            X++;
                         break;
                 }
-            }
-        }
-        public static void Render(string[,] matrix, Hero hero, string wall)
-        {
-            Console.Clear();
-            for (int i = 0; i < matrix.GetLength(0); i++)
-            {
-                for (int j = 0; j < matrix.GetLength(1); j++)
-                {
-                    if ((i == 0 || i == matrix.GetLength(0) - 1) || (j == 0 || j == matrix.GetLength(1) - 1))
-                    {
-                        Console.Write(wall);
-                    }
-                    else if (i == hero.Y && j == hero.X)
-                    {
-                        Console.Write(hero.Shape);
-                    }
-                    else
-                    {
-                        Console.Write(" ");
-                    }
-                }
-                Console.WriteLine();
+                Display();
             }
         }
         
-        public static void Main(string[] args)
+        class Wall : Element
         {
-            string[,] matrix = new string[8, 8];
-            string wall = "#";
-            Hero hero = new Hero((new Random()).Next(1, 7), (new Random()).Next(1, 7), "I");
-            
-            Console.Clear();
-
-            for (int i = 0; i < matrix.GetLength(0); i++)
+            public Wall(int x, int y)
             {
-                for (int j = 0; j < matrix.GetLength(1); j++)
-                {
-                    if ((i == 0 || i == matrix.GetLength(0) - 1) || (j == 0 || j == matrix.GetLength(1) - 1))
-                    {
-                        matrix[i, j] = wall;
-                        Console.Write(wall);
-                    }
-                    else if (i == hero.Y && j == hero.X)
-                    {
-                        matrix[i, j] = hero.Shape;
-                        Console.Write(hero.Shape);
-                    }
-                    else
-                    {
-                        matrix[i, j] = " ";
-                        Console.Write(" ");
-                    }
-                }
-                Console.WriteLine();
+                X = x;
+                Y = y;
+                Shape = "ߛ";
+                IsObstacle = true;
             }
+        }
 
+        class Ground : Element
+        {
+            public Ground(int x, int y)
+            {
+                X = x;
+                Y = y;
+                Shape = " ";
+                IsObstacle = false;
+            }
+        }
+
+        class Trap : Element
+        {
+            public Trap(int x, int y)
+            {
+                X = x;
+                Y = y;
+                Shape = "ߡ";
+                IsObstacle = true;
+            }
+        }
+        
+        class Treasure : Element
+        {
+            public Treasure(int x, int y)
+            {
+                X = x;
+                Y = y;
+                Shape = "ߋ";
+                IsObstacle = false;
+            }
+        }
+        
+        public static void Main()
+        {
+            Console.CursorVisible = false;
+            Matrix field = new Matrix(20, 20);
+            Random rng = new Random();
+            Hero hero = new Hero(rng.Next(1, 14), rng.Next(1, 14));
+            field.Render();
+            hero.Display();
             while (true)
             {
                 ConsoleKeyInfo press = Console.ReadKey();
                 if (press.Key == ConsoleKey.Q)
                     break;
-                
+                field.Render();
                 hero.Move(press);
-                Console.Beep();
-                Render(matrix, hero, wall);
             }
-
         }
     }
 }
